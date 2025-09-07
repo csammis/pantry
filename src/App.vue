8ffd1db21d5dc32b16e4c42ffa8f3523dfabe45c
Chris Sammis <csammis@gmail.com>
2025-09-06 22:14:12 -0500

    Sprucing up the nav menu a bit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import SvgIcon from '@jamescoyle/vue-icon'
-import { mdiFridgeOutline, mdiCup, mdiHomeOutline } from '@mdi/js'
+import { mdiFridgeOutline, mdiCup, mdiHomeOutline, mdiDatabaseSettingsOutline } from '@mdi/js'
 import { Slide } from 'vue3-burger-menu'
 import HomeScreen from './components/HomeScreen.vue'
 import LocationsList from './components/locations/LocationsList.vue'
@@ -13,26 +13,44 @@ interface NavMenuItem {
   hidden: boolean
 }
 
-const navMenu = ref([
+const navMenu = ref<NavMenuItem[]>([
   {
     icon: mdiHomeOutline,
     name: 'Home',
     hidden: false,
+  },
+])
+
+const settingsMenu = ref<NavMenuItem[]>([
+  {
+    icon: mdiCup,
+    name: 'Units',
+    hidden: true,
   },
   {
     icon: mdiFridgeOutline,
     name: 'Locations',
     hidden: true,
   },
-  {
-    icon: mdiCup,
-    name: 'Units',
-    hidden: true,
-  },
 ])
 
-function showElement(navItem: NavMenuItem) {
+function showNavElement(navItem: NavMenuItem) {
+  for (const item of settingsMenu.value.values()) {
+    item.hidden = true
+  }
   for (const item of navMenu.value.values()) {
+    item.hidden = true
+    if (item.name === navItem.name) {
+      item.hidden = false
+    }
+  }
+}
+
+function showSettingsElement(navItem: NavMenuItem) {
+  for (const item of navMenu.value.values()) {
+    item.hidden = true
+  }
+  for (const item of settingsMenu.value.values()) {
     item.hidden = true
     if (item.name === navItem.name) {
       item.hidden = false
@@ -47,15 +65,23 @@ function showElement(navItem: NavMenuItem) {
   </div>
   <div class="main">
     <main>
-      <Slide>
-        <span class="nav-menu-item" v-for="item in navMenu" :key="item.name">
+      <Slide :closeOnNavigation="true">
+        <div class="nav-menu-item" v-for="item in navMenu" :key="item.name">
           <svg-icon type="mdi" :path="item.icon" />
-          <a href="#" @click.stop="showElement(item)">{{ item.name }}</a>
-        </span>
+          <a href="#" @click="showNavElement(item)">{{ item.name }}</a>
+        </div>
+        <div class="nav-menu-item">
+          <svg-icon type="mdi" :path="mdiDatabaseSettingsOutline" />
+          Settings
+        </div>
+        <div class="nav-menu-item indent1" v-for="item in settingsMenu" :key="item.name">
+          <svg-icon type="mdi" :path="item.icon" />
+          <a href="#" @click="showSettingsElement(item)">{{ item.name }}</a>
+        </div>
       </Slide>
       <HomeScreen v-if="navMenu[0].hidden == false" />
-      <LocationsList v-if="navMenu[1].hidden == false" />
-      <UnitsList v-if="navMenu[2].hidden == false" />
+      <UnitsList v-if="settingsMenu[0].hidden == false" />
+      <LocationsList v-if="settingsMenu[1].hidden == false" />
     </main>
   </div>
 </template>
@@ -76,9 +102,20 @@ header {
   padding-left: 4em;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+svg {
+  width: 24px;
+  height: 24px;
+  margin-right: 0.5em;
+}
+
+.nav-menu-item {
+  padding: 0.5em;
+}
+
+.nav-menu-item,
+.nav-menu-item > a,
+.nav-menu-item > svg {
+  vertical-align: middle;
 }
 
 .nav-menu-item > a,
@@ -87,15 +124,15 @@ a:visited {
   text-decoration: none;
 }
 
+.indent1 {
+  margin-left: 1em;
+}
+
 @media (min-width: 1024px) {
   header {
     display: flex;
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
   }
 
   header .wrapper {
